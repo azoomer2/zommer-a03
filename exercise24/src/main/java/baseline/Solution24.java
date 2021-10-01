db0af61379bd9a08c5ad66e45c06e759e37b431a
Alexander <azoomer2@gmail.com>
2021-10-01 16:59:32 -0400

    E23 Skeleton Commit

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,31 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
+
+public class Solution24 {
+
+        String input1;
+        String input2;
+
+        public boolean isAnagram(String A, String B)
+        {
+                //Take inputs A and B and return true if anagram and false if not
+
+                return false;
+        }
+        public void takeInput()
+        {
+                //Takes user input and sets to string variables input1, input 2
+        }
+        public void printResult(boolean res)
+        {
+                //Prints the result to the user
+        }
+
+        public static void main(String[] args) {
+                //Call functions in proper order
+        }
+}
