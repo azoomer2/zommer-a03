8ee94ec679bcfafd716242f7ffb049b1ca028fd1
Alexander <azoomer2@gmail.com>
2021-10-02 14:23:42 -0400

    E26 Initial Commit

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,13 @@
+package baseline;
+
+public class PaymentCalculator {
+
+    public static double calculateMonthsUntilPaidOff()
+    {
+        //n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+        //Return months left
+
+        return 0.0;
+    }
+
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,21 @@
+package baseline;
+
+public class Solution26 {
+
+    public static double aprPercentToDailyRate(double percent)
+    {
+
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        //Take input for number of months
+        //Take input for APR as percent
+        double dailyRate = aprPercentToDailyRate(APRinput);
+        //Take input for balance
+        //Print out monthly payment
+    }
+
+
+
+}
