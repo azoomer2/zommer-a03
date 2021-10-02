285949c2b164c15eb6c92d8b7d27bf7feb56c6f8
Alexander <azoomer2@gmail.com>
2021-10-02 15:23:01 -0400

    E27 Initial Commit

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,40 @@
+package baseline;
+
+public class Solution27 {
+
+    private String firstName;
+    private String lastName;
+    private int zipCode;
+    private String employeeID;
+
+    public static void getFirstName()
+    {
+        //Take firstName input
+        //Check for valid input
+        //Must be filled in
+        //More than 2 characters
+    }
+    public static void getLastName()
+    {
+        //Take lastName input
+        //Check for valid input
+        //Must be filled in
+        //More than 2 characters
+    }
+    public static void getZipCode()
+    {
+        //Take zipCode input
+        //Check for valid input
+        //Must be numbers
+    }
+    public static void getEmployeeID()
+    {
+        //Take lastName input
+        //Check for valid input
+        //Must follow format two letters - four numbers
+    }
+
+    public static void main(String[] args) {
+        //Call get functions in order
+    }
+}
