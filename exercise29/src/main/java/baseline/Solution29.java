8dc94ee97eb03d3b13c8be9a767911f7a68e4f1e
Alexander <azoomer2@gmail.com>
2021-10-02 16:50:39 -0400

    E29 Initial Commit

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,36 @@
+package baseline;
+
+public class Solution29 {
+
+    private static int rateOfReturn = 0;
+
+    private static boolean inputValidation(String input){
+
+        //return true if failed validation
+        //return true if passed validation
+
+        return false;
+    }
+
+    public static int doubleInvestment(int r)
+    {
+        //years = 72/r
+
+        return years;
+    }
+
+    public static void main(String[] args) {
+
+        boolean error = false;
+
+        //take input
+
+        //validate input
+        error = inputValidation();
+        //if failed, print message
+        //else
+        doubleInvestment(input);
+
+
+    }
+}
