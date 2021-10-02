471d83638bbf54dc400a0c79437ab7729508ae3c
Alexander <azoomer2@gmail.com>
2021-10-02 16:17:35 -0400

    E28 Initial Commit

---
+++ exercise27/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+Z
+*src/test/java/baseline/Solution27Test.java,8\9\89913e3799b326b70b15dc11b1d2d6d19e392826
+V
+&src/main/java/baseline/Solution27.java,c\4\c4ea424aa0247fa241423fcc7495bda62cff644e
---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,19 @@
+package baseline;
+
+public class Solution28 {
+
+
+    public static int adder()
+    {
+        //for loop to add 5 numbers
+
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+
+        //print sum;
+        int sum = adder();
+    }
+}
