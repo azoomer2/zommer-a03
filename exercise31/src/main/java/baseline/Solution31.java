321e56a2471e11fafdca940e6cd3f92200c28125
Alexander <azoomer2@gmail.com>
2021-10-02 19:50:33 -0400

    E31 Initial Commit

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,38 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
+
+
+import java.util.Scanner;
+
+
+public class Solution31 {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    private int intensity;
+
+
+    private void takeInput()
+    {
+        //Take user input for age and resting pulse
+    }
+
+    public void calcRate(int intensity)
+    {
+        //TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+    }
+
+    private void printTable(int intensity, int rate)
+    {
+        //create table using calcRate result
+    }
+    public void main(String[] args) {
+
+        takeInput();
+
+    }
+}
