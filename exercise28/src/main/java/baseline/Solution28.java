397e2e4e638ae91b9fbaf7efb9d34eecafec6d3b
Alexander <azoomer2@gmail.com>
2021-10-02 19:29:48 -0400

    E30 Skeleton and general formatting cleanup

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -1,5 +1,8 @@
 package baseline;
-
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
 import java.util.Scanner;
 
 public class Solution28 {
---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,24 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
+public class Solution30 {
+
+    public static void tableGenerator()
+    {
+        //for loop 1-12
+            //insert new line to skip down a row
+            //for loop 1-12
+                //print i*j
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        //call tableGenerator()
+    }
+
+}
