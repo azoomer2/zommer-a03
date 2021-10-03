64c895d4232dc31a60e9c23f8779f68fe00d1c14
Alexander <azoomer2@gmail.com>
2021-10-03 16:19:17 -0400

    E36 Initial Commit

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,50 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution36 {
+    private static final Scanner scan = new Scanner(System.in);
+    static ArrayList<Integer> times = new ArrayList<Integer>();
+
+    public static double average()
+    {
+        int total;
+
+        //for loop for size of times
+        //total = loop through list and add all indices
+    }
+    public static double max()
+    {
+        int max;
+
+        //for loop for size of times
+        //if times[i] > max
+        //set max to times[i]
+
+
+    }
+    public static double min()
+    {
+        //for loop for size of times
+        //if times[i] < max
+        //set min to times[i]
+    }
+    public static double std()
+    {
+        //sd = sqrt(sumof(x-avg)^2)/times.length()))
+    }
+
+    private static void takeInput()
+    {
+        //take input from user
+        //add input to times until input = done
+    }
+
+
+    public static void main(String[] args) {
+
+        takeInput();
+
+    }
+}
---
+++ exercise36/src/test/java/baseline/Solution36Test.java
@@ -0,0 +1,34 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution36Test {
+
+    private ArrayList<Integer> test = new ArrayList<Integer>();
+
+
+
+
+
+    @Test
+    void average() {
+    }
+
+    @Test
+    void max() {
+    }
+
+    @Test
+    void min() {
+    }
+
+    @Test
+    void std() {
+    }
+
+
+}
