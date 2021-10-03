06c9bcf8e83be65e913e3967370b960acda3723e
Alexander <azoomer2@gmail.com>
2021-10-03 14:00:35 -0400

    E34 Code commit

---
+++ exercise34/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+Z
+*src/test/java/baseline/Solution34Test.java,5\e\5e48b43acfd251702de5001413690b71b63fdc8b
+V
+&src/main/java/baseline/Solution34.java,0\5\05674b502c60f30dfbe8b315f70576e1f49e3470
--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -14,16 +14,28 @@ public class Solution34 {
 
     private static void deleteEmployee(String userToDelete)
     {
-        //employee.remove(userToDelete);
-
+        //employee.remove(userToDelete)
+        employee.remove(userToDelete);
     }
 
 
 
     public static void main(String[] args) {
         //Add 5 names to employee
+        employee.add("John Smith");
+        employee.add("Jackie Jackson");
+        employee.add("Chris Jones");
+        employee.add("Amanda Cullen");
+        employee.add("Jeremy Goodwin");
+
         //prompt user for name to remove
+        System.out.print("Enter and employee name to remove: ");
+        String userInput = scan.nextLine();
+
         deleteEmployee(userInput);
+        //for loop to print remaining employees
+        for(int i=0; i<employee.size();i++)
+            System.out.print(employee.get(i)+"\n");
 
     }
 }
