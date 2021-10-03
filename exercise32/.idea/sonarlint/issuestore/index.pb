38238d227879254e4967e5e73386a908073cccb9
Alexander <azoomer2@gmail.com>
2021-10-03 12:57:46 -0400

    E32 Code Commit

---
+++ exercise32/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution32.java,b\d\bd879f0038f65cacac80a75883d242ae15f31cbf
--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -5,61 +5,162 @@ import java.util.Scanner;
 public class Solution32 {
 
     private static final Scanner scan = new Scanner(System.in);
+    private static int index = 0;
+    private static int target;
+    private static int guess;
 
 
     private static int chooseDifficulty()
     {
+        boolean invalid = true;
+        String userInput = null;
+        int diff=0;
         //Take input from user for difficulty level
+        do {
+            try {
+                    System.out.print("Enter the difficulty level (1,2 or 3): ");
+                    userInput = scan.nextLine();
+                    diff = Integer.parseInt(userInput);
+                    invalid = false;
 
+                    if(diff < 0 || diff > 3){
+                        System.out.print("Invalid input. Please try again.\n");
+                        invalid = true;
+                    }
+            } catch (Exception invalidInt) {
+                System.out.print("Invalid input. Please try again.\n");
+                invalid = true;
+            }
+        }while(invalid);
 
+        return diff;
         //return value
     }
 
-    private static boolean inputValidation(String userInput)
+    private static int firstInput()
     {
-        boolean flag = false;
+        int guess = 0;
+        boolean flag = true;
 
-        try{
-            //if non int
-        }catch{
-            //Set flag to true
-        }
-        return flag;
+        do {
+            System.out.print("I have my number. What's your guess? ");
+            String guessIn = scan.nextLine();
+            index++;
+
+            try {
+                guess = Integer.parseInt(guessIn);
+                flag = false;
+
+            } catch (Exception invalidInt) {
+                System.out.print("Invalid input. Please try again.\n");
+                flag = true;
+            }
+        }while(flag);
+
+        return guess;
     }
+
+    private static int inputValidation()
+    {
+        int guess = 0;
+        boolean flag = true;
+
+        do {
+            String guessIn = scan.nextLine();
+            index++;
+
+            try {
+                guess = Integer.parseInt(guessIn);
+                flag = false;
+
+            } catch (Exception invalidInt) {
+                System.out.print("Invalid input. Please try again.\n");
+                flag = true;
+            }
+        }while(flag);
+
+        return guess;
+    }
+    private static void guessEvaluation()
+    {
+        //while(guess != target)
+        do
+        {
+            //if guess < target
+            if(guess < target)
+            {
+                System.out.print("Too low. Guess again: ");
+                guess = inputValidation();
+            }
+            //too low message
+            //else if guess > target
+            else if(guess > target)
+            {
+                System.out.print("Too high. Guess again: ");
+                guess = inputValidation();
+            }
+            //too high message
+
+            //print you got it in index guesses!
+            //index++;
+        }while(guess != target);
+
+
+
+    }
+
 
     private static void numberGame(int difficulty)
     {
-        int target;
-        int guess;
-        int index=0;
+
+
         //Take difficulty in
         //if difficulty 1
+        if(difficulty == 1)
+        {
             //target = rand between 1 and 10
+            target = (int) (Math.random()*(9))+1;
+            System.out.println(target);
+
+        }
+
         //if difficulty 2
+        if(difficulty == 2)
+        {
             //target = rand between 1 and 100
+            target = (int) (Math.random()*(99)+1);
+        }
+
         //if difficulty 3
+        if(difficulty == 3)
+        {
             //target = rand between 1 and 1000
+            target = (int) (Math.random()*(999)+1);
+        }
+
 
         //Take user input for guess
         //inputValidation(input)
-        //while(guess != target)
-            //if guess < target
-                //too low message
-            //else if guess > target
-                //too high message
-            //else
-                //print you got it in index guesses!
-            //index++;
-
+        firstInput();
+        guessEvaluation();
+        System.out.println("You got it in "+ index + " guesses!");
 
     }
 
 
     public static void main(String[] args) {
 
-        int difficulty = chooseDifficulty();
-        numberGame(difficulty);
+        boolean tryAgain=false;
 
+        do {
+            int difficulty = chooseDifficulty();
+            numberGame(difficulty);
+            System.out.print("Do you wish to play again (Y/N)?");
+            String again = scan.nextLine();
+            if(again.matches("y"))
+                tryAgain = true;
+
+        }while(tryAgain);
 
     }
 }
