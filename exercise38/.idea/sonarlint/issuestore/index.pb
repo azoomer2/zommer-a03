65166aa1be5a95956bb553418ce79ccd7a94baed
Alexander <azoomer2@gmail.com>
2021-10-03 19:17:47 -0400

    E38 Initial Commit

---
+++ exercise38/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+V
+&src/main/java/baseline/Solution38.java,2\8\28f4056acb34e472f5f1a4471a2931afbc699333
--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -5,25 +5,52 @@ package baseline;
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution38 {
     private static final Scanner scan = new Scanner(System.in);
     private static ArrayList<Integer> evenList =  new ArrayList<>();
 
-    public static ArrayList filterEvenNumbers(String input)
+    public static void filterEvenNumbers(String inputIn)
     {
         //convert string into arrayList using strSplit
-        //if list[i]%2==0
+
+        String[] input = null;
+
+        input = inputIn.split(" ");
+
+        int size = input.length;
+        int [] arr = new int [size];
+        for(int i=0; i<size; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+            //if list[i]%2==0
+        for(int i = 0; i<size; i++)
+        {
+            if(arr[i]%2==0)
+            {
+                evenList.add(arr[i]);
+            }
+        }
             //add list[i] to evens
 
-        return evens;
     }
 
 
     public static void main(String[] args) {
         //take input as a string
-        evenList = filterEvenNumbers(userInput);
+        System.out.print("Enter a list of numbers, separated by spaces: ");
+        String userInput = scan.nextLine();
+
+         filterEvenNumbers(userInput);
         //print evenList
+        for(int i = 0; i<evenList.size(); i++)
+        {
+            System.out.print(evenList.get(i) + " ");
+        }
     }
 }
