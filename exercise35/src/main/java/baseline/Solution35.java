d66882c2ab405702952184e70a1f468b15fdf0e2
Alexander <azoomer2@gmail.com>
2021-10-03 15:14:33 -0400

    E35 Initial Commit

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,26 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution35 {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+
+    private static void listManager(int selectionIndex)
+    {
+        ArrayList<String> lottery = new ArrayList<String>();
+
+        //Prompt user to enter a name
+        //Take user input for name
+        //loop until user inputs ""
+    }
+
+
+    public static void main(String[] args) {
+        //generate selectionIndex
+            //rand num between 0 and lottery.length()
+        //print out winning name
+    }
+}
