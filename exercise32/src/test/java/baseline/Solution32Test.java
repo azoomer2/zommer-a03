9e979027bab2eb19f7b588c86e07597e8f6c2980
Alexander <azoomer2@gmail.com>
2021-10-03 13:12:51 -0400

    E33 Initial Commit

---
+++ exercise32/src/test/java/baseline/Solution32Test.java
@@ -0,0 +1,7 @@
+package baseline;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution32Test {
+    //nothing to automated test
+}
---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,36 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
+
+import java.util.Scanner;
+
+public class Solution33 {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    private static int numGenerator()
+    {
+        //generate random number between 0 and 4
+        return 0;
+    }
+
+
+
+    private static void eightBall(int randNum) {
+        //create arrayList for 4 options
+        //print option of randNum
+
+    }
+
+
+    public static void main(String[] args) {
+
+        //Take prompt for user question
+        eightBall(numGenerator());
+
+    }
+
+
+}
