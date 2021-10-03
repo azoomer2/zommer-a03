e55cb0fc1f3ed50d830018728e9f21f59ff72d40
Alexander <azoomer2@gmail.com>
2021-10-03 18:40:32 -0400

    E38 Initial Commit

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,29 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution38 {
+    private static final Scanner scan = new Scanner(System.in);
+    private static ArrayList<Integer> evenList =  new ArrayList<>();
+
+    public static ArrayList filterEvenNumbers(String input)
+    {
+        //convert string into arrayList using strSplit
+        //if list[i]%2==0
+            //add list[i] to evens
+
+        return evens;
+    }
+
+
+    public static void main(String[] args) {
+        //take input as a string
+        evenList = filterEvenNumbers(userInput);
+        //print evenList
+    }
+}
