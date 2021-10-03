586ce575ac8c27a65e74dc963e4d5836857b9378
Alexander <azoomer2@gmail.com>
2021-10-03 17:41:57 -0400

    E27 Initial Commit

---
+++ exercise32/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+Z
+*src/test/java/baseline/Solution32Test.java,b\f\bfd0fd9f1e4a9665e6d03434b09c739570d5dc8b
+V
+&src/main/java/baseline/Solution32.java,b\d\bd879f0038f65cacac80a75883d242ae15f31cbf
---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,45 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
+
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Solution37 {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static char randomSpecial()
+    {
+        //make list of special chars
+        //generate random num
+        //return random selected special char
+
+    }
+
+    public static int randomNum()
+    {
+        //make list of numbers
+        //generate random num
+        //return random selected num
+    }
+
+    public static char randomLetter()
+    {
+        //make list of letters
+        //generate random num
+        //return random selected letter
+    }
+
+    public static void main(String[] args) {
+        //Take input for special chars
+        //Call chars function
+        //Take input for numbers
+        //Call num function
+        //Call letters function for remaining
+
+        //fill ArrayList with amount of each type
+        //use .shuffle to randomly mix array into pass
+    }
+}
