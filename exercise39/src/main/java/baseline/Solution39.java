80ef2266e4cb242ec4880417d4206db5f7947838
Alexander <azoomer2@gmail.com>
2021-10-03 20:53:51 -0400

    E39 Initial Commit

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,31 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Alexander Zommer
+ */
+
+public class Solution39 {
+
+    private static void defineMaps()
+    {
+        //setup maps with given data
+    }
+
+
+    private static void sortNames()
+    {
+        //sort maps by last name
+
+    }
+
+    private static void printOutput()
+    {
+        //Format tabular output
+    }
+
+
+    public static void main(String[] args) {
+        //call appropriate functions in order
+
+    }
+}
